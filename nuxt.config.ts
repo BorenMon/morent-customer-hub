20831af41b5a07f6cc14074b90f67fd83dc90a83
Mon Boren <63392246+BorenMon@users.noreply.github.com>
2025-01-20 09:00:07 +0700

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  // devtools: { enabled: true },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   css: ['~/assets/css/main.css'],
+
   modules: [
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
-  ]
+  ],
+  devtools: {
+    enabled: true
+  }
 })
